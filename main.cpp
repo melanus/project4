35c6f3a552f7113f2a5b8936048b6725e37095b0
Matthew Taylor Flanagan <mflanag6@student01.cse.nd.edu>
2017-03-20 21:34:13 -0400

    Some main things

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
+#include <string>
+#include <queue>
+//#include <ifstream>
+#include <fstream>
 
 using namespace std;
+
+class Phrases {
+
+  public:
+	queue<string> q;
+	
+	void readFromFile(string filename) {
+		string line;
+		ifstream file;
+		file.open(filename);
+		if(file.is_open())
+		{
+			while(getline(file, line)) 
+			{
+				q.push(line);
+			}
+		}
+		else
+		{ 
+			cout << "Unable to open file " << filename << endl;
+		}
+	}
+}
 
 int main()
 {
 
+	Phrases p;
 
+	p.readFromFile("phrases.txt");
+
+	queue<string>::iterator i;
+	for(i = p.q.begin(); i != p.q.end(); i++)
+		cout << *i << endl;
 
 
 

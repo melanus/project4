030840b612c225d350cb839fc00c48279d16c7fd
Matthew Taylor Flanagan <mflanag6@student01.cse.nd.edu>
2017-03-22 23:56:36 -0400

    Reorganized

--- classes.cpp
+++ classes.cpp
@@ -10,27 +10,30 @@
 #include <queue>
 #include <condition_variable>
 
+#include "libcurl.cpp"
+
 using namespace std;
 
 //This file contains several classes used in main.cpp
 
+template <typename T>
 class SafeQueue {
 	private:
-		queue<string> q;
+		queue<T> q;
 		mutex m;
 		condition_variable cv;
 
 	public:
 		SafeQueue() {}
 
-		void push(string elem) {
+		void push(T elem) {
 			unique_lock<mutex> lock(m);
 			q.push(elem);
 			cv.notify_one();
 		}
 
-		string pop() {
-			string s;
+		T pop() {
+			T s;
 			
 			unique_lock<mutex> lock(m);
 			while(q.empty())
@@ -49,6 +52,13 @@ class SafeQueue {
 			return q.empty();
 		}
 };
+
+/*global variables*/
+SafeQueue<string> sites;
+deque<string> phrases;
+SafeQueue<pair<string, string> > siteData;
+int run;
+/* end global variables*/
 
 class Configuration {
 
@@ -90,7 +100,7 @@ class Sites {
 	
   public:
 	//deque<string> q;
-	SafeQueue q;	
+	//SafeQueue q;	
 
 	static void readFromFile(string filename) {
 		string line;
@@ -100,7 +110,7 @@ class Sites {
 		{
 			while(getline(file, line)) 
 			{
-				q.push(line);   //push_front for deque
+				sites.push(line);   //push_front for deque
 			}
 		}
 		else
@@ -113,7 +123,7 @@ class Sites {
 class Phrases {
 
   public:
-	deque<string> q;
+	//deque<string> q;
 	//SafeQueue q;	
 
 	void readFromFile(string filename) {
@@ -124,7 +134,7 @@ class Phrases {
 		{
 			while(getline(file, line)) 
 			{
-				q.push_front(line);
+				phrases.push_front(line);
 			}
 		}
 		else
@@ -134,5 +144,146 @@ class Phrases {
 	}
 };
 
+/*class Fetcher {
+	
+	public:
+		//SafeQueue data;	//this should be global
+						//the site queue should also be global
+
+	void * fetch() {
+		string site = sites.pop();
+		string html_data = libcurl(site);
+		siteData.push(html_data);
+	}
+
+};*/
+
+class Parser {
+	
+  public:
+	mutex fileLock;
+
+	void * parse() {
+		size_t pos, count;
+		string filename = to_string(run) + ".csv";
+		ofstream outputFile;
+		outputFile.open(filename, fstream::app);
+		pair<string, string> p = siteData.pop();
+		string site = p.first;
+		string data = p.second;
+		string target;
+
+		deque<string>::iterator i;
+		for(i = phrases.begin(); i != phrases.end(); i++)
+		{
+			target = *i;
+			pos = 0;
+			count = 0;
+			while(pos != -1)
+			{
+				pos = data.find(target, pos);
+				if (pos == -1) break;
+				pos++;
+				count++;
+			}
+			fileLock.lock();
+			outputFile << "Time," << target << "," << site << "," << count << endl;
+			fileLock.unlock();
+		}
+	}
+};
+	
+void libcurl(string site) {
+  CURL *curl_handle;
+  CURLcode res;
+
+  size_t pos = 0;
+  int count = 0;
+
+  struct MemoryStruct chunk;
+
+  chunk.memory = (char *)malloc(1);  /* will be grown as needed by the realloc above */
+  chunk.size = 0;    /* no data at this point */
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  /* init the curl session */
+  curl_handle = curl_easy_init();
+
+  /* specify URL to get */
+  curl_easy_setopt(curl_handle, CURLOPT_URL, site.c_str());
+
+  /* send all data to this function  */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+  /* we pass our 'chunk' struct to the callback function */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+  /* some servers don't like requests that are made without a user-agent
+     field, so we provide one */
+  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+  /* get it! */
+  res = curl_easy_perform(curl_handle);
+
+  /* check for errors */
+  if(res != CURLE_OK) {
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(res));
+  }
+  else {
+	string dat = (string)chunk.memory;
+	siteData.push(make_pair(site, dat));
+	/*string filename = to_string(run) + ".csv";
+	ofstream outputFile;
+	outputFile.open(filename, fstream::app);
+	string data = (string)chunk.memory;
+	string target;
+	deque<string>::iterator i;
+	for(i = phrases.begin(); i != phrases.end(); i++)
+	{
+		target = *i;
+		pos = 0;
+		count = 0;
+		while(pos != -1)
+		{
+			pos = data.find(target, pos);
+			if(pos == -1) { break; }
+			pos++;
+			count++;
+		}
+		outputFile << "Time," << target << "," << site << "," << count << endl;
+		//cout << target << " found " << count << " times";
+		//cout << " on site " << site << endl;
+    	//printf("%lu bytes retrieved\n", (long)chunk.size);*/
+	//}
+  }
+
+  /* cleanup curl stuff */
+  curl_easy_cleanup(curl_handle);
+
+  free(chunk.memory);
+
+  /* we're done with libcurl, so clean it up */
+  curl_global_cleanup();
+
+  //return 0;
+
+}
+
+
+class Fetcher {
+	
+	public:
+		//SafeQueue data;	//this should be global
+						//the site queue should also be global
+
+	void * fetch() {
+		string site = sites.pop();
+		libcurl(site);	//this will push onto sitesData
+	}
+
+};
+	
 
 
--- main.cpp
+++ main.cpp
@@ -3,39 +3,28 @@
 #include <fstream>
 #include <pthread.h>
 
-#include "libcurl.cpp"
-
 using namespace std;
 
 int main()
 {
-	string site;
-
-
 	Phrases p;
 	Sites s;
 	Configuration c;
+	Fetcher fetcher;
+	Parser parser;
 
 	c.readFromFile("configuration.txt");
 
 	p.readFromFile(c.data["SEARCH_FILE"]);
 
-	pthread_t test;
-	pthread_create(&test, NULL, &s.readFromFile, c.data["SITE_FILE"]);
-	//s.readFromFile(c.data["SITE_FILE"]);
-
-	pthread_join(test, NULL);
-
+	s.readFromFile(c.data["SITE_FILE"]);
 	
-	ofstream outputFile("results.csv");
-	outputFile << "Time,Phrase,Site,Count" << endl;
-	int run = 1;   //this will increment every time the timer goes
-
-
-	while(!s.q.empty()) 
+	run = 1;   //this will increment every time the timer goes
+	
+	while(!sites.empty())
 	{
-		site = s.q.pop();
-		libcurl(site, p.q, run);
-	}
+		fetcher.fetch();
+		parser.parse();
 
+	}
 }
